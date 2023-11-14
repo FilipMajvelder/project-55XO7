246d64ef9c8ab2b0a16fc5200d3bcc4563410486
FilipMajvelder <78140421+FilipMajvelder@users.noreply.github.com>
2023-11-14 21:05:22 +0100

    Update main.cpp

--- PROJEKT - METRONOM/led-matrix/02-rozsviceni_vsech_matrixu/src/main.cpp
+++ PROJEKT - METRONOM/led-matrix/02-rozsviceni_vsech_matrixu/src/main.cpp
@@ -9,12 +9,14 @@
 
 MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);   // SPI hardware interface
 
-
-unsigned char firstledfirstrow[8]={0b00000000, 0b11101110 , 0b00101010, 0b11101010, 0b10001010, 0b10101010, 0b11101110, 0b00000000};
+unsigned char firstledfirstrow[8]={0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000};
 int i=0; 
-
-unsigned char secledfirstrow[8]={0b00000000, 0b11101110 , 0b00101010, 0b11101010, 0b10001010, 0b10101010, 0b11101110, 0b00000000};
+unsigned char secledfirstrow[8]={0b00000000, 0b10001100, 0b01010010, 0b01010010, 0b01011110, 0b01010010, 0b10010010, 0b00000000};
 int k=1;
+unsigned char thrdtledfirstrow[8]={0b00000000, 0b01000011, 0b01000010, 0b01000010, 0b01000010, 0b01000010, 0b01111011, 0b00000000};
+int l=2;
+unsigned char forthledfirstrow[8]={0b00000000, 0b01111010, 0b01000010, 0b01000010, 0b01110010, 0b01000010, 0b01000010, 0b00000000};
+int m=3;
 
 void setup() 
 {
@@ -25,8 +27,9 @@ void loop()
 {
          for (int j=0; j<8; j++)
          {
-          mx.setRow(i, j, firstledfirstrow[j]);
+          mx.setRow(i, j, firstledfirstrow[j]);  
           mx.setRow(k, j, secledfirstrow[j]);
-
+          mx.setRow(l, j, thrdtledfirstrow[j]);  
+          mx.setRow(m, j, forthledfirstrow[j]);
          }
 }
