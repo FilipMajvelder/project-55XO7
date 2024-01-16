b5dc31f1c529be200590f5b4445df2cb86d20c86
FilipMajvelder <78140421+FilipMajvelder@users.noreply.github.com>
2024-01-16 19:26:05 +0100

    poznámky programátora main.cpp

--- PROJEKT - METRONOM/FINAL/05-Finalni_metronom/main.cpp
+++ PROJEKT - METRONOM/FINAL/05-Finalni_metronom/main.cpp
@@ -2,6 +2,7 @@
 #include <MD_Parola.h>
 #include "..\.pio\libdeps\nodemcu\MD_Parola\examples\Parola_Fonts\Parola_Fonts_data.h"
 
+// Definice pinů pro hardware, na kterém pracujeme, bpm, kontorleru k matici
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
 #define CLK_PIN D5
@@ -13,9 +14,11 @@
 #define MIN_BPM 30
 #define MAX_BPM 210
 
+// Inicializace knihoven pro LED matici a displej
 MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
+// Matice pro animaci kyvadla metronomu
 unsigned char poleMetronom[8][8] = {{128, 128, 128, 128, 128, 128, 128, 128},
                                     {64, 64, 64, 64, 64, 64, 64, 64},
                                     {32, 32, 32, 32, 32, 32, 32, 32},
@@ -34,13 +37,26 @@ unsigned char poleMetronomZpet[8][8] = {{1, 1, 1, 1, 1, 1, 1, 1},
                                         {64, 64, 64, 64, 64, 64, 64, 64},
                                         {128, 128, 128, 128, 128, 128, 128, 128}};
 
+// Inicializace proměnných
+bool whileSmycka = false;
+bool posledniStav = LOW;
+long casPoslednihoStisknuti = 0;
+int hodnotaPot = 0;
+int bpm = 0;
+
+// Funkce pro inicializaci zařízení
 void setup()
 {
+  // Nastavení pinů
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
+  
+  // Inicializace LED matice pro knihovnu MD_MAX72XX
   mx.begin();
   mx.control(MD_MAX72XX::INTENSITY, 5);
   mx.clear();
+
+  // Inicializace displeje pro knihovnu MD_Parola
   myDisplay.begin();
   myDisplay.setIntensity(5);
   myDisplay.displayClear();
@@ -48,12 +64,7 @@ void setup()
   myDisplay.setTextAlignment(PA_LEFT);
 }
 
-bool whileSmycka = false;
-bool posledniStav = LOW;
-long casPoslednihoStisknuti = 0;
-int hodnotaPot = 0;
-int bpm = 0;
-
+// Funkce pro zobrazení animace na LED matici
 void displayAnimace(unsigned char pattern[8], int row, int bpm)
 {
   for (int i = 0; i < 8; i++)
@@ -65,6 +76,7 @@ void displayAnimace(unsigned char pattern[8], int row, int bpm)
   mx.clear();
 }
 
+// Funkce pro zpracování stavu tlačítka
 void tlacitko()
 {
   byte stavButton = digitalRead(BUTTON_PIN);
@@ -79,6 +91,8 @@ void tlacitko()
     }
   }
 }
+
+// Funkce pro zobrazení metronomu na LED matici a zvukový signál
 void metronom()
 {
   tone(BUZZER_PIN, 1318);
@@ -108,13 +122,15 @@ void metronom()
   }
 }
 
+// Funkce pro neustálý opakování
 void loop()
 {
+  // Čtení hodnoty potenciometru a mapování na BPM
   hodnotaPot = analogRead(POTENTIOMETER_PIN);
   bpm = map(hodnotaPot, 0, 1023, MIN_BPM, MAX_BPM);
   myDisplay.print(bpm);
-  
 
+  // Zpracování tlačítka a zobrazení metronomu
   tlacitko();
   if (whileSmycka)
   {
